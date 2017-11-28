ff743d532ac6b9e400ffd9dd922d2b6beef110a1
Marcy Bursac <msbursac@gmail.com>
2017-11-28 16:39:25 -0600

    Updated Search Controller to handle "Search All" requests, built "Search All", built "List All"

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -12,9 +12,24 @@ namespace TechJobs.Controllers
             ViewBag.title = "Search";
             return View();
         }
-
         // TODO #1 - Create a Results action method to process 
-        // search request and display results
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+            ViewBag.columns = ListController.columnChoices;
+
+            if (searchType != "all")
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+
+            else
+            {
+                jobs = JobData.FindByValue(searchTerm);
+            }
+            ViewBag.jobs = jobs;
+            return View("Views/Search/Index.cshtml");            
+        }
 
     }
 }
